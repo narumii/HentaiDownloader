14a72da2da55e0d0688cf128b9efd7114708de42
narumii <narumi@waifu.club>
2020-09-27 16:26:26 +0200

    GelBooru update

--- src/main/java/xyz/ethyr/Bootstrap.java
+++ src/main/java/xyz/ethyr/Bootstrap.java
@@ -1,29 +1,18 @@
 package xyz.ethyr;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.IntStream;
+import java.io.File;
+import java.util.Scanner;
+import xyz.ethyr.downloader.impl.GelBooruDownloader;
+import xyz.ethyr.downloader.impl.NineHentaiDownloader;
 
 public class Bootstrap {
 
 
     public static void main(final String... args) {
-        final Map<Integer, Integer> imagePages = new HashMap<>();
-        final int amount = 353;
-        final int pages = amount / 1000;
-        IntStream.range(0, pages + 1).forEach(page -> {
-            if (pages == page)
-                imagePages.put(page, amount - ((page) * 1000));
-            else
-                imagePages.put(page, 1000);
-        });
-        imagePages.forEach((page, images) -> System.out.println(page + " -> " + images));
+        //final NineHentaiDownloader downloader = new NineHentaiDownloader( new File("lol"), new Scanner(System.in));
+        //downloader.downloadImages();
 
-
-        /*
-        final NineHentaiDownloader downloader = new NineHentaiDownloader( new File("lol"), new Scanner(System.in));
-
-        downloader.downloadImages();*/
+        final GelBooruDownloader downloader = new GelBooruDownloader( new File("lol"), new Scanner(System.in));
+        downloader.downloadImages();
     }
 }
--- src/main/java/xyz/ethyr/downloader/impl/GelBooruDownloader.java
+++ src/main/java/xyz/ethyr/downloader/impl/GelBooruDownloader.java
@@ -1,43 +1,102 @@
 package xyz.ethyr.downloader.impl;
 
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.IntStream;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.jsoup.Jsoup;
+import org.jsoup.internal.StringUtil;
+import org.jsoup.select.Elements;
 import xyz.ethyr.downloader.Downloader;
 
 import java.io.File;
 import java.util.Scanner;
+import xyz.ethyr.downloader.object.GelBooruSite;
+import xyz.ethyr.util.ExecutorUtil;
+import xyz.ethyr.util.FileUtil;
 
 public class GelBooruDownloader extends Downloader {
 
-    private static final String URL  = "https://gelbooru.com/index.php?page=dapi&s=post&q=index&pid=" + "%page%" + "&limit=" + "amount" + "&tags=" + "%tags%" + "&api_key=16d7195f94cd43f7680c2310ec6788f05a6a6e06fbaad1f0e6c55fd284c57f5a&user_id=629393";
+    private static final String USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11";
+    private static final String URL  = "https://gelbooru.com/index.php?page=dapi&s=post&q=index&pid=" + "%s" + "&limit=" + "%s" + "&tags=" + "%s" + "&api_key=16d7195f94cd43f7680c2310ec6788f05a6a6e06fbaad1f0e6c55fd284c57f5a&user_id=629393";
+    private final List<GelBooruSite> urls = new ArrayList<>();
     private final List<String> tags = new ArrayList<>();
     private final List<String> blockedTags = new ArrayList<>();
     private final List<String> ratings;
+    private Elements gelbooruElement;
     private final int amount;
 
     public GelBooruDownloader(final File dir, final Scanner scanner) {
         super(dir);
 
         System.out.print("Image tags: ");
-
         for (String s : scanner.nextLine().split(" ")) {
             if (s.startsWith("-"))
                 blockedTags.add(s.replace("_", ""));
             else
                 tags.add(s);
         }
+
+        System.out.print("Ratings (s q): ");
         ratings = Arrays.asList(scanner.nextLine().split(" "));
+
+        System.out.print("Amount: ");
         amount = scanner.nextInt();
 
+        final Map<Integer, Integer> imagePages = new HashMap<>();
+        final int pages = amount / 1000;
+        IntStream.range(0, pages + 1).forEach(page -> {
+            if (pages == page)
+                imagePages.put(page, amount - ((page) * 1000));
+            else
+                imagePages.put(page, 1000);
+        });
+
+        imagePages.forEach((page, images) ->  new GelBooruSite(String.format(URL, page, images, StringUtil.join(
+            Arrays.asList(tags.toArray()), "+")), page,images));
     }
 
     @Override
     public void downloadImages() {
+        ExecutorUtil.submit(()-> urls.forEach(site -> {
+            try {
+                final File file = new File(this.dir, tags.toString() + " + " + blockedTags.toString());
+                if (!file.exists())
+                    file.mkdirs();
 
+                gelbooruElement = Jsoup.connect(site.getUrl()).get().getElementsByTag("post").clone();
+                for (int i = 0; i < site.getAmount(); i++) {
+                    final Image image = getImage();
+                    if (image == null)
+                        continue;
+
+                    final URLConnection connection = new URL(image.getDownloadURL()).openConnection();
+                    connection.setRequestProperty("User-Agent", USER_AGENT);
+                    final String extension = image.getDownloadURL().split("\\.")[3];
+                    Files
+                        .copy(connection.getInputStream(), Paths.get(file.getPath(), image.getName() + "." + extension));
+                }
+            }catch (final Exception e) {
+                e.printStackTrace();
+            }
+        }));
     }
 
-    private String getImage() {
+    private Image getImage() {
         return null;
+    }
+
+    @AllArgsConstructor @Getter
+    private class Image {
+        private final String downloadURL;
+        private final String name;
     }
 }
